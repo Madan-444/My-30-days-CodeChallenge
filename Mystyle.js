fe3f7ed3910224f6c5a0b5a3ab6c6ac0e763bbdd
Madan Rijal Magar <57699836+Madan-444@users.noreply.github.com>
2019-11-16 10:38:20 +0545

    Add files via upload

---
+++ Mystyle.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Mystyle() {
+    return (
+        <div>
+        <h1 className='ClassName'>Stylesheet </h1>
+            
+        </div>
+    )
+}
+
+export default Mystyle
